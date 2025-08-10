43aafd2aa64f83e01e68234b8d68f5ab75e20f18
firmin nagnduli <finganduli@gmail.com>
2025-08-10 15:50:54 +0200

    fix: change the port from 8080 to 3002

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 3002,
+    allowedHosts: [],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
